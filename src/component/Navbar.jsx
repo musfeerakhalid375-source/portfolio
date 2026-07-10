38d2d12623e57f2811fb1e6bfc787a58f10fbcd6
musfeerakhalid375-source <musfeerakhalid375@gmail.com>
2026-07-10 17:05:38 +0500

    a

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur-md shadow-md z-50">
-      <div className="container mx-auto px-6 py-3 flex items-center justify-evenly">
+      <div className="container mx-auto px-6 py-3 flex items-center md:justify-evenly justify-between">
 
         {/* Logo */}
         <div className="logo">
